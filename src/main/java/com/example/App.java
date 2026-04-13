8c96556ead6d6978614ee93e0bb6716e45bd1637
Manjunatha H <manju.h5355@gmail.com>
2026-04-13 08:43:31 +0530

    Maven selenium file added

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,19 +4,19 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        WebDriver driver=new ChromeDriver();
-        driver.get("https://www.saucedemo.com/");
+public class App {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get( "http://automationexercise.com");
         driver.manage().window().maximize();
-        driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        driver.findElement(By.id("password")).sendKeys("secret_sauce");
-        driver.findElement(By.id("login-button")).click();
+        Thread.sleep(5000);
+        driver.findElement(By.id("username")).sendKeys("student");
+        Thread.sleep(5000);
+        driver.findElement(By.id("password")).sendKeys("Password123");
+        Thread.sleep(5000);
+        driver.findElement(By.id("submit")).click();
+        Thread.sleep(5000);
+        driver.quit();
     }
 }
